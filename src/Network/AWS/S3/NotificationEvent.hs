d7a149f756bd05b2c4bd164bab37c7dc5d792275
Jason Whittle <jason.whittle@gmail.com>
2017-01-31 17:35:18 -0500

    Add an example file from AWS documentation.

--- src/Network/AWS/S3/NotificationEvent.hs
+++ src/Network/AWS/S3/NotificationEvent.hs
@@ -1,6 +1,12 @@
 module Network.AWS.S3.NotificationEvent
-  ( Record
+  ( Record(..)
   ) where
+
+import Data.Aeson
 
 
 data Record = Record
+  deriving (Eq, Show)
+
+instance FromJSON Record where
+  parseJSON _ = pure Record
--- test/Network/AWS/S3/NotificationEventTest.hs
+++ test/Network/AWS/S3/NotificationEventTest.hs
@@ -1,5 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Network.AWS.S3.NotificationEventTest where
 
+import ClassyPrelude
+import Data.Aeson (decode')
+import Network.AWS.S3.NotificationEvent
 import Test.Tasty.Discover
 
-prop_something = 2 == 2
+
+-- http://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
+case_deserialize_happy_face :: Expectation
+case_deserialize_happy_face = do
+  raw <- readFile "examples/HappyFace.json"
+  decode' raw `shouldBe` Just Record
