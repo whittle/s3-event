786dd61607edec54e04143ef705026169d4ac150
Jason Whittle <jason.whittle@gmail.com>
2017-02-01 14:07:11 -0500

    Add simple (stupid) deserializer for S3 events.

--- src/Network/AWS/S3/NotificationEvent.hs
+++ src/Network/AWS/S3/NotificationEvent.hs
@@ -1,12 +1,116 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.AWS.S3.NotificationEvent
-  ( Record(..)
+  ( Event(..)
+  , Record(..)
+  , Bucket(..)
+  , RequestParameters(..)
+  , ResponseElements(..)
+  , S3Object(..)
+  , S3Record(..)
+  , UserIdentity(..)
   ) where
 
+import ClassyPrelude
 import Data.Aeson
 
 
+data Event = Event
+  { records :: [Record]
+  } deriving (Eq, Show)
+
+instance FromJSON Event where
+  parseJSON = withObject "S3 Event" $ \o ->
+    Event <$> o .: "Records"
+
 data Record = Record
-  deriving (Eq, Show)
+  { eventVersion :: Text
+  , eventSource :: Text
+  , awsRegion :: Text
+  , eventTime :: Text
+  , eventName :: Text
+  , userIdentity :: UserIdentity
+  , requestParameters :: RequestParameters
+  , responseElements :: ResponseElements
+  , s3Record :: S3Record
+  } deriving (Eq, Show)
 
 instance FromJSON Record where
-  parseJSON _ = pure Record
+  parseJSON = withObject "S3 Event Record" $ \o ->
+    Record <$> o .: "eventVersion"
+           <*> o .: "eventSource"
+           <*> o .: "awsRegion"
+           <*> o .: "eventTime"
+           <*> o .: "eventName"
+           <*> o .: "userIdentity"
+           <*> o .: "requestParameters"
+           <*> o .: "responseElements"
+           <*> o .: "s3"
+
+data UserIdentity = UserIdentity
+  { principalId :: Text
+  } deriving (Eq, Show)
+
+instance FromJSON UserIdentity where
+  parseJSON = withObject "User Identity" $ \o ->
+    UserIdentity <$> o .: "principalId"
+
+data RequestParameters = RequestParameters
+  { sourceIPAddress :: Text
+  } deriving (Eq, Show)
+
+instance FromJSON RequestParameters where
+  parseJSON = withObject "Request Parameters" $ \o ->
+    RequestParameters <$> o .: "sourceIPAddress"
+
+data ResponseElements = ResponseElements
+  { xAmzRequestId :: Text
+  , xAmzId2 :: Text
+  } deriving (Eq, Show)
+
+instance FromJSON ResponseElements where
+  parseJSON = withObject "Response Elements" $ \o ->
+    ResponseElements <$> o .: "x-amz-request-id"
+                     <*> o .: "x-amz-id-2"
+
+data S3Record = S3Record
+  { s3SchemaVersion :: Text
+  , configurationId :: Text
+  , bucket :: Bucket
+  , object :: S3Object
+  } deriving (Eq, Show)
+
+instance FromJSON S3Record where
+  parseJSON = withObject "S3 Record" $ \o ->
+    S3Record <$> o .: "s3SchemaVersion"
+             <*> o .: "configurationId"
+             <*> o .: "bucket"
+             <*> o .: "object"
+
+data Bucket = Bucket
+  { name :: Text
+  , ownerIdentity :: UserIdentity
+  , arn :: Text
+  } deriving (Eq, Show)
+
+instance FromJSON Bucket where
+  parseJSON = withObject "Bucket" $ \o ->
+    Bucket <$> o .: "name"
+           <*> o .: "ownerIdentity"
+           <*> o .: "arn"
+
+data S3Object = S3Object
+  { key :: Text
+  , size :: Int
+  , eTag :: Text
+  , versionId :: Text
+  , sequencer :: Text
+  } deriving (Eq, Show)
+
+instance FromJSON S3Object where
+  parseJSON = withObject "S3 Object" $ \o ->
+    S3Object <$> o .: "key"
+             <*> o .: "size"
+             <*> o .: "eTag"
+             <*> o .: "versionId"
+             <*> o .: "sequencer"
--- test/Network/AWS/S3/NotificationEventTest.hs
+++ test/Network/AWS/S3/NotificationEventTest.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Network.AWS.S3.NotificationEventTest where
 
@@ -9,7 +10,78 @@ import Test.Tasty.Discover
 
 
 -- http://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
-case_deserialize_happy_face :: Expectation
-case_deserialize_happy_face = do
+case_deserialize_happy_face_example :: Expectation
+case_deserialize_happy_face_example = do
   raw <- readFile "examples/HappyFace.json"
-  decode' raw `shouldBe` Just Record
+  let expected = Record
+        { eventVersion = "2.0"
+        , eventSource = "aws:s3"
+        , awsRegion = "us-east-1"
+        , eventTime = "1970-01-01T00:00:00.000Z"
+        , eventName = "ObjectCreated:Put"
+        , userIdentity = UserIdentity
+          { principalId = "AIDAJDPLRKLG7UEXAMPLE"
+          }
+        , requestParameters = RequestParameters
+          { sourceIPAddress = "127.0.0.1"
+          }
+        , responseElements = ResponseElements
+          { xAmzRequestId = "C3D13FE58DE4C810"
+          , xAmzId2 = "FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
+          }
+        , s3Record = S3Record
+          { s3SchemaVersion = "1.0"
+          , configurationId = "testConfigRule"
+          , bucket = Bucket
+            { name = "mybucket"
+            , ownerIdentity = UserIdentity
+              { principalId = "A3NL1KOZZKExample"
+              }
+            , arn = "arn:aws:s3:::mybucket"
+            }
+          , object = S3Object
+            { key = "HappyFace.jpg"
+            , size = 1024
+            , eTag = "d41d8cd98f00b204e9800998ecf8427e"
+            , versionId = "096fKKXTRTtl3on89fVO.nfljtsv6qko"
+            , sequencer = "0055AED6DCD90281E5"
+            }
+          }
+        }
+  decode' raw `shouldBe` Just (Event [expected])
+
+case_deserialize_user_identity :: Expectation
+case_deserialize_user_identity =
+  let json = "{\"principalId\":\"AIDAJDPLRKLG7UEXAMPLE\"}"
+      expected = UserIdentity "AIDAJDPLRKLG7UEXAMPLE"
+  in decode' json `shouldBe` Just expected
+
+case_deserialize_request_parameters :: Expectation
+case_deserialize_request_parameters =
+  let json = "{\"sourceIPAddress\":\"127.0.0.1\"}"
+      expected = RequestParameters "127.0.0.1"
+  in decode' json `shouldBe` Just expected
+
+case_deserialize_response_elements :: Expectation
+case_deserialize_response_elements =
+  let json = "{\"x-amz-request-id\":\"C3D13FE58DE4C810\",\"x-amz-id-2\":\"FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD\"}"
+      expected = ResponseElements "C3D13FE58DE4C810" "FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
+  in decode' json `shouldBe` Just expected
+
+case_deserialize_s3_record :: Expectation
+case_deserialize_s3_record =
+  let json = "{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"testConfigRule\",\"bucket\":{\"name\":\"mybucket\",\"ownerIdentity\":{\"principalId\":\"A3NL1KOZZKExample\"},\"arn\":\"arn:aws:s3:::mybucket\"},\"object\":{\"key\":\"HappyFace.jpg\",\"size\":1024,\"eTag\":\"d41d8cd98f00b204e9800998ecf8427e\",\"versionId\":\"096fKKXTRTtl3on89fVO.nfljtsv6qko\",\"sequencer\":\"0055AED6DCD90281E5\"}}"
+      expected = S3Record "1.0" "testConfigRule" (Bucket "mybucket" (UserIdentity "A3NL1KOZZKExample") "arn:aws:s3:::mybucket") (S3Object "HappyFace.jpg" 1024 "d41d8cd98f00b204e9800998ecf8427e" "096fKKXTRTtl3on89fVO.nfljtsv6qko" "0055AED6DCD90281E5")
+  in decode' json `shouldBe` Just expected
+
+case_deserialize_bucket :: Expectation
+case_deserialize_bucket =
+  let json = "{\"name\":\"mybucket\",\"ownerIdentity\":{\"principalId\":\"A3NL1KOZZKExample\"},\"arn\":\"arn:aws:s3:::mybucket\"}"
+      expected = Bucket "mybucket" (UserIdentity "A3NL1KOZZKExample") "arn:aws:s3:::mybucket"
+  in decode' json `shouldBe` Just expected
+
+case_deserialize_S3_object :: Expectation
+case_deserialize_S3_object =
+  let json = "{\"key\":\"HappyFace.jpg\",\"size\":1024,\"eTag\":\"d41d8cd98f00b204e9800998ecf8427e\",\"versionId\":\"096fKKXTRTtl3on89fVO.nfljtsv6qko\",\"sequencer\":\"0055AED6DCD90281E5\"}"
+      expected = S3Object "HappyFace.jpg" 1024 "d41d8cd98f00b204e9800998ecf8427e" "096fKKXTRTtl3on89fVO.nfljtsv6qko" "0055AED6DCD90281E5"
+  in decode' json `shouldBe` Just expected
